67b5a176dc8722e048dde0f9a5defdf316269289
javadocq <han18264@naver.com>
2025-09-12 02:55:20 +0900

    feat: Button component

---
+++ src/shared/ui/Button.tsx
@@ -0,0 +1,67 @@
+/**
+ * 버튼 컴포넌트
+ *
+ * @param {Object} props - 컴포넌트 props
+ * @param {string} props.label - 버튼의 레이블
+ * @param {string} props.size - 버튼의 크기 (medium | large)
+ * @param {string} props.disabled - disabled 상태 여부
+ * @param {string} props.variant - 버튼의 타입 (primary | secondary)
+ * @param {() => void} props.onClick - 클릭 이벤트 핸들러
+ *
+ * @example
+ * // 기본 버튼
+ * <Button label="확인" onClick={() => console.log("clicked")} />
+ *
+ * @example
+ * // Large Secondary 버튼
+ * <Button
+ *   label="취소"
+ *   size="large"
+ *   variant="secondary"
+ *   onClick={() => console.log("취소됨")}
+ * />
+ *
+ * @example
+ * // Disabled 버튼
+ * <Button label="비활성" disabled />
+ */
+
+interface ButtonProps {
+  label?: string;
+  size?: "medium" | "large";
+  disabled?: boolean;
+  variant?: "primary" | "secondary";
+  onClick?: () => void;
+}
+
+export function Button({ label, size, disabled, variant = "primary", onClick }: ButtonProps) {
+  const variantStyle =
+    variant === "primary"
+      ? disabled
+        ? "bg-gray100"
+        : "bg-primary400"
+      : disabled
+        ? "bg-gray50"
+        : "bg-gray100";
+
+  const textStyle =
+    variant === "primary"
+      ? disabled
+        ? "text-gray400"
+        : "text-gray0"
+      : disabled
+        ? "text-gray400"
+        : "text-gray800";
+  return (
+    <button
+      type="button"
+      className={`flex items-center justify-center py-[14px] rounded-[8px] gap-[10px] transition-color duration-300 cursor-pointer ${
+        size === "large" ? "w-[335px]" : "w-[160px]"
+      } ${variantStyle}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <p className={`whitespace-nowrap text-subtitle1 truncate ${textStyle}`}>{label}</p>
+    </button>
+  );
+}
