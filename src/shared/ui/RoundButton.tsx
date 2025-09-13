e11de68af0ad854c47b4c646910abc73c9654f67
javadocq <han18264@naver.com>
2025-09-13 23:08:29 +0900

    feat: support dynamic icon rendering with iconName prop

--- src/shared/ui/RoundButton.tsx
+++ src/shared/ui/RoundButton.tsx
@@ -2,31 +2,26 @@
  * 원형 버튼 컴포넌트
  *
  * @param {Object} props - 컴포넌트 props
- * @param {boolean} [props.isIconShow=false] - 아이콘 표시 여부
+ * @param {IconName} [props.iconName] - 아이콘 이름
  * @param {string} props.label - 버튼의 레이블
  * @param {() => void} props.onClick - 클릭 이벤트 핸들러
  *
  * @example
- * <RoundButton icon={<YourIcon />} onClick={() => console.log("clicked")} />
+ * <RoundButton iconName="speaker" label="스피커" onClick={() => console.log("clicked")} />
  */
 
 import { cn } from "@/shared/lib/utils";
-import { Icon } from "./Icon";
+import { Icon, IconName } from "./Icon";
 
 interface RoundButtonProps {
-  isIconShow?: boolean;
+  iconName?: IconName;
   label?: string;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
 }
 
-export function RoundButton({
-  isIconShow = false,
-  label,
-  type = "button",
-  onClick,
-}: RoundButtonProps) {
-  const paddingStyle = isIconShow ? "pl-2 pr-3" : "px-3";
+export function RoundButton({ iconName, label, type = "button", onClick }: RoundButtonProps) {
+  const paddingStyle = iconName ? "pl-2 pr-3" : "px-3";
   return (
     <button
       type={type}
@@ -36,7 +31,7 @@ export function RoundButton({
       )}
       onClick={onClick}
     >
-      {isIconShow && <Icon name="speaker" title="버튼" size={20} />}
+      {iconName && <Icon name={iconName} title="버튼" size={20} className="text-gray0" />}
       <p className="text-body2-semibold truncate text-gray0">{label}</p>
     </button>
   );
