e620a4e746c97f663323b5708ac195ec6ac485c4
sooyeon <lllllh00@naver.com>
2025-09-21 16:27:19 +0900

    feat: implement map page componets (#31)
    * feat: add caption1-bold text variables to design system
    
    * feat: add pin components
    
    * feat: add map location componet
    
    * feat: add map refresh button, floating button
    
    * refactor: add gap
    
    * chore: rename triangle icon
    
    * chore: update Icon name from "triangleDown" to "downTriangle"

---
+++ src/entities/map/ui/MapLocation.tsx
@@ -0,0 +1,8 @@
+export function MapLocation() {
+  return (
+    <div className="relative">
+      <div className="h-[54px] w-[54px] rounded-full bg-primary400 opacity-30" />
+      <div className="absolute top-[17px] left-[17px] box-border h-5 w-5 rounded-full border-4 border-gray0 bg-primary400 shadow-[0_4px_12px_0_rgba(0,0,0,0.15)]" />
+    </div>
+  );
+}
