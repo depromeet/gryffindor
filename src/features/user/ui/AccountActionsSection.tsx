b12a202e922aec53ce668bbf0e17dbe0d8776af9
해진(HaejinYun) <86779590+haejinyun@users.noreply.github.com>
2025-09-25 00:41:44 +0900

    feat: add mypage logout flow and userState flow

--- src/features/user/ui/AccountActionsSection.tsx
+++ src/features/user/ui/AccountActionsSection.tsx
@@ -1,15 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { useUserState } from "@/entities/user";
+import { LogoutConfirmModal } from "@/features/auth/ui";
+
 export function AccountActionsSection() {
-  //todo: 모달띄우는는 로직 필요.
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+
+  const { userState } = useUserState();
+
+  if (!userState.isLoggedIn) {
+    return null;
+  }
 
   return (
-    <div className="m-auto mb-[40px] flex w-fit items-center gap-x-[16px]">
-      <button type="button" className="text-body2-medium text-gray600">
-        로그아웃
-      </button>
-      <div className="h-full w-[1px] bg-gray100 py-[4px]" />
-      <button type="button">
-        <span className="text-body2-medium text-gray600">회원탈퇴</span>
-      </button>
-    </div>
+    <>
+      <div className="m-auto mb-[40px] flex w-fit items-center gap-x-[16px]">
+        <button
+          type="button"
+          className="text-body2-medium text-gray600"
+          onClick={() => {
+            console.log("로그아웃");
+            setIsLogoutModalOpen(true);
+          }}
+        >
+          로그아웃
+        </button>
+        <div className="h-full w-[1px] bg-gray100 py-[4px]" />
+        <button type="button">
+          <span className="text-body2-medium text-gray600">회원탈퇴</span>
+        </button>
+      </div>
+      <LogoutConfirmModal
+        isOpen={isLogoutModalOpen}
+        onOpenChange={() => setIsLogoutModalOpen((prev) => !prev)}
+      />
+    </>
   );
 }
