8de5cf7bb9c69b43178ae23e26d0b5c51b45d80f
soosoo22 <lllllh00@naver.com>
2025-09-24 13:06:31 +0900

    refactor: wrapped with StoreListBottomSheet

--- src/entities/storeList/ui/StoreList.tsx
+++ src/entities/storeList/ui/StoreList.tsx
@@ -1,22 +1,17 @@
 import Link from "next/link";
-import { STORE_LIST_MOCK_DATA } from "../model";
+import type { StoreListResponse } from "../api";
 import { StoreCard } from "./StoreCard";
-import { StoreLevelList } from "./StoreLevelList";
 
-export function StoreList() {
+export function StoreList({ storeList }: { storeList: StoreListResponse[] }) {
   return (
-    <div className="flex flex-col bg-gray0 px-[20px]">
-      <StoreLevelList />
-      <ul className="flex flex-col gap-y-[15px]">
-        {STORE_LIST_MOCK_DATA.map((store) => (
-          <li key={store.id}>
-            <Link href={`#`}>
-              {/* fixme: Link 수정 필요 */}
-              <StoreCard {...store} />
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="flex flex-col gap-y-[15px]">
+      {storeList.map((store) => (
+        <li key={store.id}>
+          <Link href={`#`}>
+            <StoreCard {...store} />
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
---
+++ src/features/map/ui/StoreListBottomSheet.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useMemo } from "react";
+import type { StoreListResponse } from "@/entities/storeList/api";
+import { FloatingStoreCard, StoreList } from "@/entities/storeList/ui";
+import {
+  BottomSheet,
+  BottomSheetContent,
+  BottomSheetHandler,
+  BottomSheetHeader,
+} from "@/shared/ui";
+
+interface StoreListBottomSheetProps {
+  storeList: StoreListResponse[];
+  selectedStoreId: number | null;
+}
+
+export function StoreListBottomSheet({ storeList, selectedStoreId }: StoreListBottomSheetProps) {
+  const selectedStoreInfo = useMemo(
+    () => storeList.find((store) => store.id === selectedStoreId),
+    [storeList, selectedStoreId],
+  );
+
+  if (selectedStoreInfo) return <FloatingStoreCard {...selectedStoreInfo} />;
+
+  return (
+    <BottomSheet isFixed={false} isOpen={true} initialHeight={226} expandedOffset={88}>
+      <BottomSheetHeader>
+        <BottomSheetHandler />
+      </BottomSheetHeader>
+      <BottomSheetContent className="px-5 pb-9">
+        <StoreList storeList={storeList} />
+      </BottomSheetContent>
+    </BottomSheet>
+  );
+}
