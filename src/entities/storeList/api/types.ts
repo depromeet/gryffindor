f8c84d085dfb5546e8e7a4879c66f3b33dbee151
soosoo22 <lllllh00@naver.com>
2025-09-25 15:22:35 +0900

    refactor: rename interface to StoreSearchResponse

--- src/entities/storeList/api/types.ts
+++ src/entities/storeList/api/types.ts
@@ -1,6 +1,6 @@
 export type SeatTypes = "FOR_ONE" | "FOR_TWO" | "FOR_FOUR" | "CUBICLE" | "BAR_TABLE";
 
-export interface StoreListResponse {
+export interface StoreSearchResponse {
   id: number;
   name: string;
   thumbnailUrl: string;
@@ -8,6 +8,6 @@ export interface StoreListResponse {
   coordinate: { lat: number; lon: number };
   distance: number;
   walkingMinutes: number;
-  seats: SeatTypes[];
+  seatTypes: SeatTypes[];
   honbobLevel: number;
 }
