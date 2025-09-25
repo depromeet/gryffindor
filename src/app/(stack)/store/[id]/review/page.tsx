8cc1b50e06be4ff5031276a06b169f44ecb49cfa
javadocq <han18264@naver.com>
2025-09-26 02:18:33 +0900

    feat: add modal

--- src/app/(stack)/store/[id]/review/page.tsx
+++ src/app/(stack)/store/[id]/review/page.tsx
@@ -2,7 +2,7 @@
 
 import { useParams, useSearchParams } from "next/navigation";
 import { useState } from "react";
-import { FilterSection, TransitionLayout } from "@/shared/ui";
+import { FilterSection, Icon, Modal, TransitionLayout } from "@/shared/ui";
 import { CTA } from "@/shared/ui/CTA";
 import { InputReview } from "@/shared/ui/InputReview";
 
@@ -38,6 +38,7 @@ export default function ReviewPage() {
 
   const [selectedFilters, setSelectedFilters] = useState<string[]>(getInitialFilters());
   const [review, setReview] = useState(initialContent);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleFilterChange = (items: string[]) => {
     setSelectedFilters(items);
@@ -54,6 +55,7 @@ export default function ReviewPage() {
     };
 
     console.log(payload);
+    setIsModalOpen(true);
   };
 
   return (
@@ -81,6 +83,35 @@ export default function ReviewPage() {
             primaryDisabled={!(selectedFilters.length && review.length >= 5)}
           />
         </div>
+        {isModalOpen && (
+          <Modal
+            isOpen={isModalOpen}
+            onOpenChange={setIsModalOpen}
+            title="후기 작성 완료"
+            description="후기 작성이 완료되었습니다."
+          >
+            <div className="flex min-w-[260px] flex-col items-center justify-center gap-6 rounded-lg bg-white p-4">
+              <button
+                type="button"
+                className="mb-[-24px] flex h-5 w-full justify-end"
+                onClick={() => setIsModalOpen(false)}
+              >
+                <Icon name="close" size={24} className="cursor-pointer text-gray400" />
+              </button>
+              <Icon name="lv4User" size={120} disableCurrentColor />
+              <span className="whitespace-pre-line text-center text-gray900 text-subtitle1">
+                {isEditMode ? "방문 후기를 수정했어요!" : `방문 후기\n작성이 완료되었어요!`}
+              </span>
+              <button
+                type="button"
+                onClick={() => setIsModalOpen(false)}
+                className="flex h-[51px] w-full items-center justify-center rounded-[8px] bg-primary400 py-[14px]"
+              >
+                <span className="text-body1-semibold text-gray0">확인</span>
+              </button>
+            </div>
+          </Modal>
+        )}
       </div>
     </TransitionLayout>
   );
