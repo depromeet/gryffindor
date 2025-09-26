7657197ef814373e90ce242ef50e6dc0cce676b6
해진(HaejinYun) <86779590+haejinyun@users.noreply.github.com>
2025-09-26 17:56:39 +0900

    feat: delete unnecessary tag

--- src/entities/honbob/ui/HonbobLevelCard.tsx
+++ src/entities/honbob/ui/HonbobLevelCard.tsx
@@ -16,26 +16,24 @@ export function HonbobLevelCard({
   recommendedStore,
 }: HonbobLevelCardProps) {
   return (
-    <div className="flex items-center justify-between gap-x-[10px] rounded-[16px] bg-gray0 px-[20px] py-[16px]">
-      <div className="flex flex-col gap-y-[4px]">
-        <div className="flex items-center gap-x-[8px]">
-          <Tag label={honbobLevel} color="red" size="small" />
-          <span className="justify-start text-body1-semibold text-gray900">{honbobLevelTitle}</span>
+    <div className="flex flex-col justify-between gap-x-[10px] gap-y-[4px] rounded-[16px] bg-gray0 px-[20px] py-[16px]">
+      <div className="flex items-center gap-x-[8px]">
+        <Tag label={honbobLevel} color="red" size="small" />
+        <span className="justify-start text-body1-semibold text-gray900">{honbobLevelTitle}</span>
+      </div>
+      <span className="justify-start text-body3-semibold text-primary400">
+        {honbobLevelDescription}
+      </span>
+      <div className="flex flex-col gap-y-[2px]">
+        <div className="flex gap-x-[4px] text-body3-regular text-gray900">
+          <span>추천 메뉴</span>
+          <span>:</span>
+          <span>{recommendedMenu}</span>
         </div>
-        <span className="justify-start text-body3-semibold text-primary400">
-          {honbobLevelDescription}
-        </span>
-        <div className="flex flex-col gap-y-[2px]">
-          <div className="flex gap-x-[4px] text-body3-regular text-gray900">
-            <span>추천 메뉴</span>
-            <span>:</span>
-            <span>{recommendedMenu}</span>
-          </div>
-          <div className="flex gap-x-[4px] text-body3-regular text-gray900">
-            <span>추천 식당</span>
-            <span>:</span>
-            <span>{recommendedStore}</span>
-          </div>
+        <div className="flex gap-x-[4px] text-body3-regular text-gray900">
+          <span>추천 식당</span>
+          <span>:</span>
+          <span>{recommendedStore}</span>
         </div>
       </div>
     </div>
