70ab7de1dfe83930e3f1ddc83ae2139be56e828a
해진(HaejinYun) <86779590+haejinyun@users.noreply.github.com>
2025-09-26 14:26:30 +0900

    fix: add missing code when main branch merge

--- src/shared/api/queryKeys.ts
+++ src/shared/api/queryKeys.ts
@@ -1,4 +1,5 @@
 export const queryKeys = {
+  STORE_DETAIL: (storeId: string) => ["store", storeId],
   USER_PROFILE: () => ["user", "profile"],
   USER_NICKNAME: () => ["user", "nickname"],
   STORE_LIST: (station: string, level: number) => ["store", "list", station, level],
