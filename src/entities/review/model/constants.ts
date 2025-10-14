801ddeb099e4cd460da39e0043cb0518841043bf
javadocq <han18264@naver.com>
2025-10-14 21:08:57 +0900

    fix: change to constant data

--- src/entities/review/model/constants.ts
+++ src/entities/review/model/constants.ts
@@ -1,9 +1,9 @@
 export const REVIEW_KEYWORD_EN_KO_MAP: Record<string, string> = {
-  GOOD_FOR_SOLO: "🏃‍♂️ 빠른 회전률",
-  OPEN_ATMOSPHERE: "✨ 넓은 매장",
+  FAST_TURNOVER: "🏃‍♂️ 빠른 회전률",
+  SPACIOUS_STORE: "✨ 넓은 매장",
   KIND_SERVICE: "😊 친절한 응대",
-  NICE_STAFF: "💰 저렴한 한끼",
-  BEST_TASTE: "👍 보증된 맛",
+  AFFORDABLE_MEAL: "💰 저렴한 한끼",
+  GUARANTEED_TASTE: "👍 보증된 맛",
 };
 
 export const REVIEW_FILTERS = Object.values(REVIEW_KEYWORD_EN_KO_MAP);
