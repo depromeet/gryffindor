ab64660eaa0b2c61f9d850e0a0dc1989f4e09121
Salvation_sub <kyman19940214@gmail.com>
2025-10-28 23:23:33 +0900

    refactor: transition

--- web/src/app/_providers/SsgoiProvider.tsx
+++ web/src/app/_providers/SsgoiProvider.tsx
@@ -4,19 +4,15 @@ import { Ssgoi } from "@ssgoi/react";
 import type { PropsWithChildren } from "react";
 import { useMemo } from "react";
 import { createSsgoiConfig } from "@/shared/config";
-import { useMobile, useNavigationDirectionContext } from "@/shared/lib";
+import { useMobile } from "@/shared/lib";
 
 interface SsgoiProviderProps extends PropsWithChildren {}
 
 export function SsgoiProvider({ children }: SsgoiProviderProps) {
   const isMobile = useMobile();
-  const navigationDirection = useNavigationDirectionContext();
 
   // Config 생성 최적화: 캐싱된 transition rules를 사용하므로 빠름
-  const config = useMemo(
-    () => createSsgoiConfig(isMobile, navigationDirection),
-    [isMobile, navigationDirection],
-  );
+  const config = useMemo(() => createSsgoiConfig(isMobile), [isMobile]);
 
   return (
     <Ssgoi config={config}>
