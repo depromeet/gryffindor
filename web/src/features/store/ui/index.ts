4ff6a34fe003010dcc8a77129e81aab4eefa3821
Lee, Jong Won <138106799+javadocq@users.noreply.github.com>
2025-11-04 22:00:28 +0900

    feat: zoom in on image click (#74)
    * feat: zoomable
    
    * refactor: menusection
    
    * feat: duration image zoom
    
    * feat: favicon

--- web/src/features/store/ui/index.ts
+++ web/src/features/store/ui/index.ts
@@ -1,5 +1,7 @@
+export * from "./MenuSection";
 export * from "./ReviewList";
 export * from "./SeatImageGallery";
 export * from "./SeatInfoSection";
 export * from "./StoreInfo";
 export * from "./SuggestionCard";
+export * from "./ZoomableImage";
