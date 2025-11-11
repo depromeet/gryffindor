dcb0ede047e381a5df48364f84a6c38e7a864b18
javadocq <han18264@naver.com>
2025-11-11 15:26:31 +0900

    feat: supporturl

--- web/src/entities/user/model/constant.ts
+++ web/src/entities/user/model/constant.ts
@@ -1,6 +1,10 @@
 import type { IconName } from "@/shared/ui";
 
-export const MY_PAGE_NAVIGATION: { label: string; icon: IconName; href: string }[] = [
+export const MY_PAGE_NAVIGATION: {
+  label: string;
+  icon: IconName;
+  href: string;
+}[] = [
   {
     label: "내 방문 후기 보기",
     icon: "colorNote",
@@ -10,5 +14,10 @@ export const MY_PAGE_NAVIGATION: { label: string; icon: IconName; href: string }
     label: "레벨 테스트 하기",
     icon: "colorTest",
     href: "/level-test",
+  },
+  {
+    label: "문의하러 가기",
+    icon: "colorSpeaker",
+    href: "https://forms.gle/5FnfNnyP7Rx41o64A",
   },
 ];
