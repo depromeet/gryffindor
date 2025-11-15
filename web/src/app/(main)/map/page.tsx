25612d3ef663b13cbcbb1d7ed3e246894904a188
soosoo22 <lllllh00@naver.com>
2025-11-15 15:04:20 +0900

    refactor: update toast message when no store data

--- web/src/app/(main)/map/page.tsx
+++ web/src/app/(main)/map/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect } from "react";
 import {
   useLocation,
   useMapCoordinate,
@@ -19,7 +18,6 @@ import {
   StoreBottomSheet,
 } from "@/features/map/ui";
 import { SearchBar } from "@/features/search";
-import { useToast } from "@/shared/lib/hooks";
 import { useFilterStore } from "@/shared/store";
 import { TransitionLayout } from "@/shared/ui";
 
@@ -29,10 +27,8 @@ export default function MapPage() {
   const { requestLocation } = useLocation(map);
   const { isDragging, resetDragging } = useMapDrag(map);
 
-  const { storeList, isFetching, isSearchMode } = useStoreListData({ bounds, center });
+  const { storeList, isFetching } = useStoreListData({ bounds, center });
   const { isFilterOpen, filters, openFilter, closeFilter, setFilters } = useFilterStore();
-
-  const { showToast } = useToast();
 
   const levelDisplayValue =
     filters.honbobLevel.length > 1 ? "커스텀" : `레벨${filters.honbobLevel[0] || 1}`;
@@ -41,12 +37,6 @@ export default function MapPage() {
     updateCoordinate(map);
     resetDragging();
   };
-
-  useEffect(() => {
-    if (!isSearchMode && !isFetching && storeList.length === 0) {
-      showToast({ message: "현재는 강남·역삼 지역만 이용할 수 있어요." });
-    }
-  }, [isSearchMode, isFetching, storeList.length, showToast]);
 
   return (
     <TransitionLayout>
